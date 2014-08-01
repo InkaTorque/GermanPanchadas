20bade2b6d7e5d88be1c70860eb9083e3fea08d4
InkaTorque <fama1994@gmail.com>
2014-08-01 16:31:09 -0500

    Cuadrado que se mueve

---
+++ Last Chance/Assets/Scripts/MovimientoScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("d"))
+		{
+			rigidbody2D.velocity = new Vector2(5,0);
+		}
+		if(Input.GetKey("space"))
+		{
+			rigidbody2D.velocity = new Vector2(0,5);
+		}
+		if(Input.GetKey("s"))
+		{
+			rigidbody2D.velocity = new Vector2(0,-5);
+		}
+		if(Input.GetKey("a"))
+		{
+			rigidbody2D.velocity = new Vector2(-5,0);
+
+		}
+		if (Input.GetKeyUp ("d") || Input.GetKeyUp ("a")) {
+			rigidbody2D.velocity=new Vector2(0,0);
+		}
+
+	}
+}
