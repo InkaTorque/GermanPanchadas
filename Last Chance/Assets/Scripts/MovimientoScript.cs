6a353282a9c0eda7b935029d01f88d51cc09470a
Prisoner19 <clz.german@gmail.com>
2014-08-01 18:12:59 -0500

    Basic jump, dude

--- Last Chance/Assets/Scripts/MovimientoScript.cs
+++ Last Chance/Assets/Scripts/MovimientoScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MovimientoScript : MonoBehaviour {
@@ -8,6 +8,9 @@ public class MovimientoScript : MonoBehaviour {
 	
 	private bool moviendo_izq;
 	private bool moviendo_der;
+
+	private bool saltando;
+	private bool doble_salto;
 	
 	// Use this for initialization
 	void Start () 
@@ -17,6 +20,9 @@ public class MovimientoScript : MonoBehaviour {
 		
 		moviendo_izq = false;
 		moviendo_der = false;
+
+		saltando = false;
+		doble_salto = false;
 	}
 	
 	// Update is called once per frame
@@ -28,58 +34,68 @@ public class MovimientoScript : MonoBehaviour {
 	
 	private void verificar_movimiento()
 	{
-		if(Input.GetKey("d"))
+		if(Input.GetKeyDown("d"))
+		{
+			vel_x = 5;
+			moviendo_der = true;
+			moviendo_izq = false;
+		}
+		else if(Input.GetKeyDown("a"))
+		{
+			vel_x = -5;
+			moviendo_izq = true;
+			moviendo_der = false;
+		}
+		else if(Input.GetKey("d") && moviendo_der == true)
 		{
 			vel_x = 5;
 		}
-		else if(Input.GetKey("a"))
+		else if(Input.GetKey("a") && moviendo_izq == true)
 		{
 			vel_x = -5;
 		}
-		else if(Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+		else if(Input.GetKeyUp ("d"))
 		{
 			vel_x = 0;
+			moviendo_der = false;
+			moviendo_izq = true;
+		}
+		else if(Input.GetKeyUp ("a"))
+		{
+			vel_x = 0;
+			moviendo_izq = false;
+			moviendo_der = true;
 		}
 		
-		
-		//		if(Input.GetKeyDown("d"))
-		//		{
-		//			vel_x = 5;
-		//			moviendo_der = true;
-		//			moviendo_izq = false;
-		//		}
-		//		else if(Input.GetKeyDown("a"))
-		//		{
-		//			vel_x = -5;
-		//			moviendo_izq = true;
-		//			moviendo_der = false;
-		//		}
-		//		else if(Input.GetKey("d") && moviendo_der == true)
-		//		{
-		//			vel_x = 5;
-		//		}
-		//		else if(Input.GetKey("a") && moviendo_izq == true)
-		//		{
-		//			vel_x = -5;
-		//		}
-		//		else if(Input.GetKeyUp ("d"))
-		//		{
-		//			vel_x = 0;
-		//			moviendo_der = false;
-		//			moviendo_izq = true;
-		//		}
-		//		else if(Input.GetKeyUp ("a"))
-		//		{
-		//			vel_x = 0;
-		//			moviendo_izq = false;
-		//			moviendo_der = true;
-		//		}
-		
-		rigidbody2D.velocity = new Vector2(vel_x,vel_y);
+		rigidbody2D.velocity = new Vector2(vel_x,rigidbody2D.velocity.y);
+
+		Debug.Log(rigidbody2D.velocity.x);
 	}
 	
 	private void verificar_salto()
 	{
-		
+		if(Input.GetKeyDown("space") && (saltando == false || doble_salto == false))
+		{
+			vel_y = 5;
+			rigidbody2D.velocity = new Vector2(vel_x, vel_y);
+
+			if(saltando == false)
+			{
+				saltando = true;
+			}
+			else
+			{
+				doble_salto = true;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag.Equals("Platform") == true)
+		{
+			saltando = false;
+			doble_salto = false;
+		}
 	}
 }
