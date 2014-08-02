803d4e500dbb08729a6f832cb71fa68564dfbc03
Prisoner19 <clz.german@gmail.com>
2014-08-02 01:29:46 -0500

    Shooting fixed, kid

--- Last Chance/Assets/Scripts/Ataque.cs
+++ Last Chance/Assets/Scripts/Ataque.cs
@@ -1,21 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ataque : MonoBehaviour {
+public class Ataque : MonoBehaviour 
+{
 	public Transform BulletPrefab;
+	private float time_holding;
 	//Vector3 posicion;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		time_holding = 0;
 		//posicion = GetComponent<Transform> ().localPosition;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey("j"))
+	void Update () 
+	{
+		verificar_disparo();
+	}
+
+	private void verificar_disparo()
+	{
+		if(Input.GetKeyDown("j"))
 		{
 			generarBala();
 		}
+		else if(Input.GetKey("j"))
+		{
+			hold();
+			if(time_holding > 0.2f)
+			{
+				generarBala();
+			}
+		}
+		else if(Input.GetKeyUp("j"))
+		{
+			unhold();
+		}
 	}
+
+	private void hold()
+	{
+		time_holding += Time.deltaTime;
+	}
+
+	private void unhold()
+	{
+		time_holding = 0;
+	}
+
 	public void generarBala()
 	{
 		//posicion.Set (posicion.x+32, posicion.y, posicion.z);
